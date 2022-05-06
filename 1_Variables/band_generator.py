44fdff0220469e1d642cd67c76d5365a3e36b110
lucasseridonio <lucasseridonio@gmail.com>
2022-05-05 22:00:08 -0300

    Day 1: Variables

---
+++ 1_Variables/band_generator.py
@@ -0,0 +1,14 @@
+#1. Create a greeting for your program.
+print('Welcome to the app Band Name Generator\n\n')
+
+#2. Ask the user for the city that they grew up in.
+city = input('What city did you grow up in?\n')
+
+#3. Ask the user for the name of a pet.
+pet_name = input("Enter a pet's name: \n")
+
+#4. Combine the name of their city and pet and show them their band name.
+band = city + ' ' + pet_name
+print(band)
+
+#5. Make sure the input cursor shows on a new line.
